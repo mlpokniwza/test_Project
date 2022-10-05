e01130c2813c4cef3467b6a5358642639477eb07
Phanyapat Chalermchai <kayzazz1@hotmail.com>
2022-10-05 11:42:29 +0700

    initial commit

---
+++ receive_POST.php
@@ -0,0 +1,4 @@
+<?php
+  $user_name=$_POST['user'];
+  echo"Receive user name is$user_name";
+?>
---
+++ sent_POST.php
@@ -0,0 +1,6 @@
+<form action="receive_POST.php"method="post">
+Username:
+<input type="text"name="user">
+<input type="submit"value="Send">
+<input type="reset"value="Clear">
+</form>
